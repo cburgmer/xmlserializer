32df1e34e15d68d684144202786505b68d4bea45
Christoph Burgmer <cburgmer@ira.uka.de>
2013-09-16 21:59:48 +0200

    CDATA

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Package for the browser
 
 $ npm install browserify
 $ node ./node_modules/browserify/bin/cmd.js lib/web.js -o dist/html2xhtml.js
+
+Run tests
+---------
+
+$ npm install
+$ ./node_modules/jasmine-node/bin/jasmine-node tests
--- tests/converterSpec.js
+++ tests/converterSpec.js
@@ -1,15 +1,25 @@
 var html2xhtml = require('../lib/converter').html2xhtml;
 
 describe('html2xhtml', function () {
-    var withXHTMLBoilerplate = function (content) {
-        return '<html xmlns="http://www.w3.org/1999/xhtml"><head/><body>' +
-            content +
-            '</body></html>';
-    }
-    var emptyDocument = function () {
-        return '<html xmlns="http://www.w3.org/1999/xhtml"><head/><body/></html>';
+    var withXHTMLBoilerplate = function (body, head) {
+        var document = '<html xmlns="http://www.w3.org/1999/xhtml">';
+        if (head) {
+            document += '<head>' + head + '</head>';
+        } else {
+            document += '<head/>';
+        }
+        if (body) {
+            document += '<body>' + body + '</body>';
+        } else {
+            document += '<body/>';
+        }
+        document += '</html>';
+        return document;
     }
 
+    var emptyDocument = function () {
+        return withXHTMLBoilerplate();
+    }
 
     it('should return a valid XHTML document for empty input', function() {
         expect(html2xhtml('')).toEqual(emptyDocument());
@@ -51,9 +61,29 @@ describe('html2xhtml', function () {
         expect(xhtml).toEqual(withXHTMLBoilerplate('<p class="myClass"> </p>'));
     });
 
+    it('should serialize HTML enties', function () {
+        var xhtml = html2xhtml('&ndash;');
+
+        expect(xhtml).toEqual(withXHTMLBoilerplate('–'));
+    });
+
     it('should serialize to self closing attribute', function () {
         var xhtml = html2xhtml('<br/>');
 
         expect(xhtml).toEqual(withXHTMLBoilerplate('<br/>'));
+    });
+
+    it('should put script content into CDATA blocks', function () {
+        var xhtml = html2xhtml('<script>var a = 1 & 1;</script>');
+
+        expect(xhtml).toEqual(withXHTMLBoilerplate('',
+            '<script><![CDATA[\nvar a = 1 & 1;\n]]></script>'));
+    });
+
+    it('should put script content into CDATA blocks', function () {
+        var xhtml = html2xhtml('<style>span:before { content: "<"; }</style>');
+
+        expect(xhtml).toEqual(withXHTMLBoilerplate('',
+            '<style><![CDATA[\nspan:before { content: "<"; }\n]]></style>'));
     });
 });
