b6f2a4a4ff0fc3bae8bcae657e4e0ad11b23269f
Christoph Burgmer <cburgmer@ira.uka.de>
2013-11-13 18:53:56 +0100

    Fix special case with empty text node

---
+++ tests/browserSpecs/integrationSpec.js
@@ -0,0 +1,11 @@
+var parser = window.parser,
+    serializer = window.xmlserializer;
+
+describe('xmlserializer.js', function () {
+    it("should not choke on null text node value", function () {
+        var doc = document.implementation.createHTMLDocument("");
+        doc.body.innerHTML = "Test content";
+
+        expect(serializer.serializeToString(doc)).toMatch(/<title><\/title>/);
+    });
+});
