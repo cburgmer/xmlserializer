36b194a9c1e74939a225cfb0150fa9c627a683b0
Kevin Locke <kevin@kevinlocke.name>
2014-09-25 21:20:41 -0600

    Output an xmlns attribute on namespace changes
    Whenever the namespace of an element does not match the namespace of its
    parent (or when it doesn't have a parent from which to inherit a
    namespace), an xmlns attribute must be output to declare the namespace
    of the element.
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- tests/browserSpecs/integrationSpec.js
+++ tests/browserSpecs/integrationSpec.js
@@ -8,4 +8,18 @@ describe('xmlserializer', function () {
 
         expect(serializer.serializeToString(doc)).toMatch(/<title><\/title>/);
     });
+
+    it("should output xmlns attributes for namespace changes", function () {
+        var doc = document.implementation.createHTMLDocument("");
+        var div = doc.createElementNS("http://www.w3.org/1999/xhtml", "div");
+        var foreignObject =
+            doc.createElementNS("http://www.w3.org/2000/svg", "foreignObject");
+        foreignObject.appendChild(div);
+        var svg = doc.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.appendChild(foreignObject);
+        doc.body.appendChild(svg);
+
+        expect(serializer.serializeToString(doc))
+            .toMatch(/<body><svg xmlns="http:\/\/www.w3.org\/2000\/svg"><foreignObject><div xmlns="http:\/\/www.w3.org\/1999\/xhtml"( *\/>|><\/div>)<\/foreignObject><\/svg><\/body>/);
+    });
 });
