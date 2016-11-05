5b50d9fc9e008e60584e4114f6fe88b88310bc7b
Christoph Burgmer <christoph.burgmer@gmail.com>
2016-11-06 00:26:19 +0800

    Upgrade to Jasmine2 from npm, fix waitFor statement

--- tests/run-jasmine.js
+++ tests/run-jasmine.js
@@ -56,7 +56,7 @@ page.open(system.args[1], function(status){
     } else {
         waitFor(function(){
             return page.evaluate(function(){
-                return document.body.querySelector('.symbolSummary .pending') === null
+                return document.body.querySelector('.jasmine-suite-detail') !== null;
             });
         }, function(){
             var exitCode = page.evaluate(function(){
